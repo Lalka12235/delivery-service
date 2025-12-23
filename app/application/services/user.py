6b05c7f9110f88670abc38975116193714631ab3
Lalka12235 <fisher12235@mail.ru>
2025-12-23 13:53:11 +0700

    feat: delete uncessary logic from user service and add coordinates entity

--- app/application/services/user.py
+++ app/application/services/user.py
@@ -1,5 +1,5 @@
 from app.domain.entity.user import UserID
-from app.domain.exception.user import UserNotFound, UserAlreadyExist
+from app.domain.exception.user import UserNotFound
 from app.domain.interfaces.user import UserRepository
 from app.domain.entity import UserEntity
 
@@ -8,7 +8,6 @@ class UserService:
 
     def __init__(self,user_repo: UserRepository):
         self.user_repo = user_repo
-
 
     def get_user_by_id(self,user_id: UserID) -> UserEntity:
         user = self.user_repo.get_user_by_id(user_id)
@@ -25,25 +24,16 @@ class UserService:
         return user
 
     def create_user(self,user: UserEntity) -> UserEntity:
-        user_exist = self.user_repo.get_user_by_id(user.id)
-        if user_exist:
-            raise UserAlreadyExist()
-
-        user_created = self.user_repo.create_user(user)
-        return user_created
+        return self.user_repo.create_user(user)
 
     def update_user(self,user: UserEntity) -> bool:
-        user_exist = self.user_repo.get_user_by_id(user.id)
-        if not user_exist:
+        updated = self.user_repo.update_user(user)
+        if not updated:
             raise UserNotFound()
-
-        user_updated = self.user_repo.update_user(user)
-        return user_updated
+        return updated
 
     def delete_user(self,user_id: UserID) -> UserEntity:
-        user_exist = self.user_repo.get_user_by_id(user_id)
-        if not user_exist:
+        deleted = self.user_repo.delete_user(user_id)
+        if not deleted:
             raise UserNotFound()
-        
-        user_deleted = self.user_repo.delete_user(user_id)
-        return user_deleted
+        return deleted
--- app/domain/entity/address.py
+++ app/domain/entity/address.py
@@ -5,6 +5,11 @@ from typing import NewType
 AddressID = NewType('AddressID',uuid.UUID)
 
 @dataclass(slots=True,frozen=True)
+class Coordinates:
+    lat: float
+    lng: float
+
+@dataclass(slots=True,frozen=True)
 class AddressEntity:
     id: AddressID
     city: str
@@ -12,5 +17,6 @@ class AddressEntity:
     house_number: str
     apartment_number: str
     floor: int
+    coords: Coordinates
     user_id: uuid.UUID | None
     restaurant_id: uuid.UUID | None
