150588d61f6d02fcf71e119437903c2e562369f7
Lalka12235 <fisher12235@mail.ru>
2025-12-24 13:17:16 +0700

    feat: make base exception

--- app/domain/exception/base_exception.py
+++ app/domain/exception/base_exception.py
@@ -1,7 +1,42 @@
-class UserNotFound(Exception):
-    def __init__(self,detail='Пользователь не найден',**kwargs):
-        super().__init__(detail,kwargs)
+class NotFoundError(Exception):
+    entity_name: str = "Entity"
 
-class UserAlreadyExist(Exception):
-    def __init__(self,detail='Пользователь уже существует',**kwargs):
-        super().__init__(detail,kwargs)
+    def __init__(self,**kwargs):
+        self.message = f"{self.entity_name} не найден"
+        super().__init__(self.message,kwargs)
+
+class AlreadyExists(Exception):
+    entity_name: str = 'Object'
+
+    def __init__(self,**kwargs):
+        self.message = f"{self.entity_name} уже существует"
+        super().__init__(self.message,kwargs)
+
+class UserNotFoundError(NotFoundError):
+    entity_name = "User"
+
+class ShopNotFoundError(NotFoundError):
+    entity_name = "Shop"
+
+class ReviewNotFoundError(NotFoundError):
+    entity_name = "Review"
+
+class OrderNotFoundError(NotFoundError):
+    entity_name = "Order"
+
+
+
+class UserAlreadyExists(AlreadyExists):
+    entity_name: str = 'User'
+
+class ShopAlreadyExists(AlreadyExists):
+    entity_name: str = 'Shop'
+
+class ReviewAlreadyExists(AlreadyExists):
+    entity_name: str = 'Review'
+
+
+class AccessDenied(Exception):
+
+    def __init__(self, detail: str = 'В доступе отказано', **kwargs):
+        super().__init__(detail, kwargs)
