81a4d24e9e6a94498c181d9e9d30fd9468534ad7
Lalka12235 <fisher12235@mail.ru>
2026-01-09 13:13:01 +0700

    feat:  add user model

---
+++ app/infrastructure/db/models/__init__.py
@@ -0,0 +1,8 @@
+all = (
+    'Base',
+    'UserModel',
+
+)
+
+from app.infrastructure.db.models.base import Base
+from app.infrastructure.db.models.user import UserModel
---
+++ app/infrastructure/db/models/user.py
@@ -0,0 +1,21 @@
+from datetime import datetime,UTC
+
+from app.domain.enum import RoleType
+from app.infrastructure.db.models import Base
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import UUID,DateTime,ForeignKey
+import uuid
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=True)
+    last_name: Mapped[str | None] = mapped_column(nullable=False,default=None)
+    date_birth: Mapped[datetime] = mapped_column(DateTime(timezone=True),nullable=True)
+    email: Mapped[str | None] = mapped_column(nullable=False,default=None)
+    phone_number: Mapped[str] = mapped_column(nullable=True)
+    role: Mapped[RoleType] = mapped_column(nullable=True,default=RoleType.DEFAULT)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),nullable=True,default=datetime.now(UTC))
+    address_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('address.id'),nullable=True)
